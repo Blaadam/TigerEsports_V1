3967a747c151d2fc8f68b89643135ac92fb11fb3
Adam <adam.worden06@gmail.com>
2024-11-28 10:40:02 +0000

    Login Page & Home Page

---
+++ TigerEsports_V1/Validation/LoginPasswordValidation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TigerEsports_V1.Validation
+{
+    public class LoginPasswordValidation : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += BindableOnTextChanged;
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= BindableOnTextChanged;
+        }
+        private async void BindableOnTextChanged(object? sender, TextChangedEventArgs e)
+        {
+            var Password = e.NewTextValue;
+            var entry = sender as Entry;
+
+            if (entry != null)
+            {
+                ValidationVault.LoginPassword = IsPasswordValid(Password);
+                entry.TextColor = ValidationVault.LoginPassword ? Color.FromArgb("000000") : Color.FromArgb("FF0000");
+            }
+        }
+        private bool IsPasswordValid(string password)
+        {
+#if DEBUG
+            return password.Length >= 8;
+
+#else
+            return Regex.IsMatch(password, "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
+#endif
+        }
+    }
+}
--- TigerEsports_V1/Views/LoginPage.xaml.cs
+++ TigerEsports_V1/Views/LoginPage.xaml.cs
@@ -1,17 +1,57 @@
-namespace TigerEsports_V1.Views;
+using TigerEsports_V1.Data;
 using TigerEsports_V1.Databases;
+using TigerEsports_V1.Validation;
 
+namespace TigerEsports_V1.Views;
 public partial class LoginPage : ContentPage
 {
     private UserDatabase _database;
     public LoginPage(UserDatabase database)
     {
-		InitializeComponent();
+        InitializeComponent();
         _database = database;
     }
 
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
-		Application.Current.MainPage = new RegisterPage(_database);
+        Application.Current.MainPage = new RegisterPage(_database);
+    }
+
+    private async void LoginBtn_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(UsernameTb.Text) || string.IsNullOrEmpty(UsernameTb.Text.Trim()))
+        {
+            await DisplayAlert("Registration Error", "You need to enter a username before you can log int.", "Ok");
+            return;
+        }
+
+        if (!ValidationVault.LoginPassword)
+        {
+            await DisplayAlert("Registration Error", "You need to enter a valid password before you can log in.", "Ok");
+            return;
+        }
+
+        string Username = UsernameTb.Text.Trim();
+        string Password = PasswordTb.Text.Trim();
+
+        var users = await _database.SearchUsersAsync(Username);
+        bool isAuthenticated = false;
+
+        foreach (User user in users)
+        {
+            if (user.Password == Password)
+            {
+                isAuthenticated = true;
+                break;
+            }
+        }
+
+        if (!isAuthenticated)
+        {
+            await DisplayAlert("Registration Error", "Your credentials are incorrect. Please try again.", "Ok");
+            return;
+        }
+
+        Application.Current.MainPage = new AppShell();
     }
 }
